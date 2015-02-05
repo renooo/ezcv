931d77b393fab3578f40a01ad67a749e6433c19d
Renaud <renaud.bougre@gmail.com>
2015-02-05 18:56:19 +0100

    edition exp et missions

--- app/scripts/controllers/editexperience.js
+++ app/scripts/controllers/editexperience.js
@@ -11,37 +11,43 @@ angular.module('ezcvApp')
   .controller('EditexperienceCtrl', function ($scope, $rootScope, $location, $routeParams, $q, Job, Company) {
   	$scope.me = $rootScope.me;
   	$scope.experience = null;
+    //$scope.experienceOriginal = null;
   	$scope.jobs = [];
   	$scope.companies = [];
   	$scope.loading = true;
 
-  	if(!angular.isDefined($scope.me) ){
-  		$location.path('/edit');
-  		return;
-  	}
-
-  	$scope.experience = $scope.me._embedded.experiences.filter(function(experience){
-  		return (experience.id == $routeParams.experienceId);
-  	})[0];
-
-  	if(!angular.isDefined($scope.experience)){
-  		$location.path('/edit');
-  		return;
-  	}
-
-  	$q.all([
-  		Job.get({}, function(jobs){
-  			$scope.jobs = jobs._embedded.jobs;
-  		}).$promise,
-
-  		Company.get({}, function(companies){
-  			$scope.companies = companies._embedded.companies;
-  		})
-  	]).then(function(){
-  		$scope.loading = false;
-  	});
-
   	$scope.editCV = function(){
-  		$location.path('/edit');
+      //if(angular.equals($scope.experience, $scope.experienceOriginal)){
+  		  location.path('/edit');
+      //}
   	};
+    
+    if(!angular.isDefined($scope.me) ){
+      $location.path('/edit');
+      return;
+    }
+
+    $scope.experience = $scope.me._embedded.experiences.filter(function(experience){
+      return (experience.id == $routeParams.experienceId);
+    })[0];
+
+    if(!angular.isDefined($scope.experience)){
+      $location.path('/edit');
+      return;
+    }
+
+    $q.all([
+      Job.get({}, function(jobs){
+        $scope.jobs = jobs._embedded.jobs;
+      }).$promise,
+
+      Company.get({}, function(companies){
+        $scope.companies = companies._embedded.companies;
+      }).$promise
+
+    ]).then(function(){
+        //$scope.experienceOriginal = angular.copy($scope.experience);
+        $scope.loading = false;
+    });
+
   });
